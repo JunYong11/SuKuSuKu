c9073bad8cdc9fa97465acc0af7c29a47d3d46eb
이준용 <wnsdyd1375@naver.com>
2025-02-28 13:40:22 +0900

    2025-02-28
    v1

---
+++ src/main/java/com/web/sukusuku/SukusukuApplication.java
@@ -0,0 +1,13 @@
+package com.web.sukusuku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukusukuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SukusukuApplication.class, args);
+	}
+
+}
