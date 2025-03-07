b26769889fec82f5695406bec1355a28f7e06165
jiyeong <duddl1016@naver.com>
2025-03-07 13:02:13 +0900

    2025.03.07 기본틀 완성

--- src/main/java/com/web/sukusuku/controller/homeController.java
+++ src/main/java/com/web/sukusuku/controller/homeController.java
@@ -2,7 +2,6 @@ package com.web.sukusuku.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
