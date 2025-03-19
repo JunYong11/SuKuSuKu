1de1b4d54e250e09890928aba621e199c94a0e68
イジュンヨン（이준용_釜山） <92434317+JunYong11@users.noreply.github.com>
2025-03-19 12:50:32 +0900

    2025-03-19

--- src/main/java/com/web/sukusuku/service/MyPageService.java
+++ src/main/java/com/web/sukusuku/service/MyPageService.java
@@ -1,5 +1,12 @@
 package com.web.sukusuku.service;
 
-public class MyPageService {
+import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class MyPageService {
+	
+	
 }
